bb09b4d8d8643b62daca20a7f232479d93c7a07f
anis-ktari <71829876+anis-ktari@users.noreply.github.com>
2020-11-24 11:16:04 +0100

    Create an empty EvenOrOdd Project

---
+++ src/EvenOrOddKata/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EvenOrOddKata
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class EvenOrOdd
+    {
+        //
+        
+
+    }
+}
---
+++ src/EvenOrOddKataTest/EvenOrOddKataTest.cs
@@ -0,0 +1,22 @@
+using EvenOrOddKata;
+using NUnit.Framework;
+
+namespace EvenOrOddKataTest
+{
+    [TestFixture]
+    public class EvenOrOddKataTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            EvenOrOdd evenOrOddKata = new EvenOrOdd();
+
+            Assert.Pass();
+        }
+    }
+}
